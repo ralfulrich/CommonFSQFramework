7245cc9c7d1eb659003bf8942e1c95d164b53b1b
makbiyik <melike.akbiyik@kit.edu>
2015-06-30 16:36:00 +0200

    add env script

---
+++ Core/test/HaloMuonAna/setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#
+export SmallXAnaDefFile="/afs/cern.ch/work/m/makbiyik/public/HALOMUON/CMSSW_7_3_5/src/CommonFSQFramework/Core/test/HaloMuonAna/MyAnalysis.py"
+export SmallXAnaVersion="CommonFSQFramework.Skim.Samples_MuonAna_0T_20150624"
+export HaloMuonOutput="/afs/cern.ch/work/m/makbiyik/public/HALOMUON/CMSSW_7_3_5/src/CommonFSQFramework/Core/test/HaloMuonAna/output"
+source /cvmfs/cms.cern.ch/crab3/crab.sh
+grid-proxy-init
+voms-proxy-init --voms cms 
