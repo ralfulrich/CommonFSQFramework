eb2012f6c3fb5bdd282896dec0eecd89a45fc60b
Clemens <Hauke.Woehrmann@kit.edu>
2015-11-23 14:15:41 +0100

    initialize new muon sample on 2015E data

--- Skim/python/Samples_MuonAna_0T_20150717.py
+++ Skim/python/Samples_MuonAna_0T_20150717.py
@@ -36,6 +36,21 @@ sam["data_MinimumBias_MuonHLTSkim_Run2015A"]["XS"]=-1
 sam["data_MinimumBias_MuonHLTSkim_Run2015A"]["pathPAT"]='/XXXTMFPAT/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/151030_111657/0000//'
 sam["data_MinimumBias_MuonHLTSkim_Run2015A"]["DS"]='/MinimumBias/Run2015A-PromptReco-v1/RECO'
 
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]={}
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["crabJobs"]=232
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["GT"]='GR_R_75_V5A'
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["name"]='data_Cosmics_MuonHLTSkim_Run2015E'
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["isData"]=True
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["numEvents"]=4486825
+# sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathSE"]='srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/eos/cms/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/151030_111657/0000/'
+# sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathTrees"]='/XXXTMFTTree/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/151030_111657/0000//'
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["json"]='CommonFSQFramework/Skim/lumi/Cosmics_CastorMuonRuns.json'
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["lumiMinBias"]=-1
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["XS"]=-1
+# sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathPAT"]='/XXXTMFPAT/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/151030_111657/0000//'
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["DS"]='/Cosmics/Run2015E-PromptReco-v1/RECO'
+
+
 def fixLocalPaths(sam):
         import os,imp
         if "SmallXAnaDefFile" not in os.environ:
