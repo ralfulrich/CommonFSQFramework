307a2d75d5e6b492d76051daaeb1b3656c2f1368
Clemens <Hauke.Woehrmann@kit.edu>
2015-11-25 10:41:57 +0100

    add CFF trees directory; now able to run python script on CFF trees

--- Skim/python/Samples_MuonAna_0T_20150717.py
+++ Skim/python/Samples_MuonAna_0T_20150717.py
@@ -42,12 +42,12 @@ sam["data_Cosmics_MuonHLTSkim_Run2015E"]["GT"]='GR_R_75_V5A'
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["name"]='data_Cosmics_MuonHLTSkim_Run2015E'
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["isData"]=True
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["numEvents"]=4486825
-# sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathSE"]='srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/eos/cms/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/151030_111657/0000/'
-# sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathTrees"]='/XXXTMFTTree/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/151030_111657/0000//'
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathSE"]='srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/eos/cms/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/Cosmics/MuonAna_0T_20150717_data_Cosmics_MuonHLTSkim_Run2015E/151124_172833/0000/'
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathTrees"]='/XXXTMFTTree/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/Cosmics/MuonAna_0T_20150717_data_Cosmics_MuonHLTSkim_Run2015E/151124_172833/0000//'
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["json"]='CommonFSQFramework/Skim/lumi/Cosmics_CastorMuonRuns.json'
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["lumiMinBias"]=-1
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["XS"]=-1
-# sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathPAT"]='/XXXTMFPAT/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/151030_111657/0000//'
+sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathPAT"]='/XXXTMFPAT/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/Cosmics/MuonAna_0T_20150717_data_Cosmics_MuonHLTSkim_Run2015E/151124_172833/0000//'
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["DS"]='/Cosmics/Run2015E-v1/RAW'
 
 
