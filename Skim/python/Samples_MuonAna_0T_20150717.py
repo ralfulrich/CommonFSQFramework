10220044d8a25625fcbaf3a4709b110a0a54c960
Clemens <Hauke.Woehrmann@kit.edu>
2015-12-11 10:55:35 +0100

    add comments to samples file to make properties of muon samples more clear

--- Skim/python/Samples_MuonAna_0T_20150717.py
+++ Skim/python/Samples_MuonAna_0T_20150717.py
@@ -8,6 +8,8 @@ skimEfficiencyMethod="getSkimEff"
 
 sam = {}
 
+# CFF Skim using interfill data from run 2015A
+# No HLT filter selection was done
 sam["data_MinimumBias_Run2015A"]={}
 sam["data_MinimumBias_Run2015A"]["crabJobs"]=1609
 sam["data_MinimumBias_Run2015A"]["GT"]='GR_R_75_V5A'
@@ -22,6 +24,10 @@ sam["data_MinimumBias_Run2015A"]["XS"]=-1
 sam["data_MinimumBias_Run2015A"]["pathPAT"]='/XXXTMFPAT/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/150717_095008/0000//'
 sam["data_MinimumBias_Run2015A"]["DS"]='/MinimumBias/Run2015A-PromptReco-v1/RECO'
 
+# CFF Skim using interfill data from run 2015A
+# Filtered on CastorMuon HLT path:
+#     HLT_L1CastorMuon_v1
+#  OR HLT_L1Tech59_CASTORHaloMuon_v1
 sam["data_MinimumBias_MuonHLTSkim_Run2015A"]={}
 sam["data_MinimumBias_MuonHLTSkim_Run2015A"]["crabJobs"]=1609
 sam["data_MinimumBias_MuonHLTSkim_Run2015A"]["GT"]='GR_R_75_V5A'
@@ -36,6 +42,15 @@ sam["data_MinimumBias_MuonHLTSkim_Run2015A"]["XS"]=-1
 sam["data_MinimumBias_MuonHLTSkim_Run2015A"]["pathPAT"]='/XXXTMFPAT/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/MinimumBias/MuonAna_0T_20150717_data_MinimumBias_Run2015A/151030_111657/0000//'
 sam["data_MinimumBias_MuonHLTSkim_Run2015A"]["DS"]='/MinimumBias/Run2015A-PromptReco-v1/RECO'
 
+# CFF Skim using interfill data from run 2015E
+# during pp-Reference-Run
+# This data is stored in the Cosmic dataset therefore it needed to run 
+# on RAW data (Cosmics/RECO has no Castor RecHit producer included). 
+# Need also to copy dataset from tape to T2_BE_IIHE to access it.
+# MagnetField: Undefined 
+#              (Most propable 4T but in the end also 0T events could went in)
+# Filtered on CastorMuon HLT path:
+#     HLT_L1Tech59_CastorMuon_v1
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]={}
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["crabJobs"]=232
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["GT"]='75X_dataRun2_Prompt_ppAt5TeV_v0'
@@ -50,6 +65,15 @@ sam["data_Cosmics_MuonHLTSkim_Run2015E"]["XS"]=-1
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["pathPAT"]='/XXXTMFPAT/tier2/store/user/cwohrman/Cosmics/MuonAna_0T_20150717_data_Cosmics_MuonHLTSkim_Run2015E/151126_133520/0000/store/group/phys_heavyions/cwohrman/CFF/CastorMuon/Cosmics/MuonAna_0T_20150717_data_Cosmics_MuonHLTSkim_Run2015E/151124_172833/0000//'
 sam["data_Cosmics_MuonHLTSkim_Run2015E"]["DS"]='/Cosmics/Run2015E-v1/RAW'
 
+
+# CFF Skim using interfill data from run 2015E
+# during pp-Reference-Run
+# This data is stored in the Cosmic dataset therefore it needed to run 
+# on RAW data (Cosmics/RECO has no Castor RecHit producer included). 
+# Need also to copy dataset from tape to T2_BE_IIHE to access it.
+# MagnetField: 4T
+# Filtered on CastorMuon HLT path:
+#     HLT_L1Tech59_CastorMuon_v1
 sam["data_Cosmics_MuonHLTSkim_2015E_4T"]={}
 sam["data_Cosmics_MuonHLTSkim_2015E_4T"]["crabJobs"]=232
 sam["data_Cosmics_MuonHLTSkim_2015E_4T"]["GT"]='75X_dataRun2_Prompt_ppAt5TeV_v0'
@@ -64,6 +88,14 @@ sam["data_Cosmics_MuonHLTSkim_2015E_4T"]["XS"]=-1
 sam["data_Cosmics_MuonHLTSkim_2015E_4T"]["pathPAT"]='/XXXTMFPAT/tier2/store/user/cwohrman/CFF/CastorMuon/Cosmics/MuonAna_0T_20150717_data_Cosmics_MuonHLTSkim_2015E_4T/151210_095015/0000//'
 sam["data_Cosmics_MuonHLTSkim_2015E_4T"]["DS"]='/Cosmics/Run2015E-v1/RAW'
 
+# CFF Skim using interfill data from run HI 2015E
+# the first part of HI data
+# This data is stored in the Cosmic dataset therefore it needed to run 
+# on RAW data (Cosmics/RECO has no Castor RecHit producer included). 
+# Need also to copy dataset from tape to T2_BE_IIHE to access it.
+# MagnetField: 0T
+# Filtered on CastorMuon HLT path:
+#     HLT_L1Tech59_CastorMuon_v1
 sam["data_Cosmics_MuonHLTSkim_2015E_0T"]={}
 sam["data_Cosmics_MuonHLTSkim_2015E_0T"]["crabJobs"]=232
 sam["data_Cosmics_MuonHLTSkim_2015E_0T"]["GT"]='75X_dataRun2_Prompt_ppAt5TeV_v0'
@@ -78,6 +110,14 @@ sam["data_Cosmics_MuonHLTSkim_2015E_0T"]["XS"]=-1
 sam["data_Cosmics_MuonHLTSkim_2015E_0T"]["pathPAT"]='/XXXTMFPAT/tier2/store/user/cwohrman/CFF/CastorMuon/Cosmics/MuonAna_0T_20150717_data_Cosmics_MuonHLTSkim_2015E_0T/151210_195631/0000//'
 sam["data_Cosmics_MuonHLTSkim_2015E_0T"]["DS"]='/Cosmics/Run2015E-v1/RAW'
 
+
+# CFF Skim using interfill data from run HI 2015E
+# This data is stored in the Cosmic dataset therefore it needed to run 
+# on RAW data (Cosmics/RECO has no Castor RecHit producer included). 
+# Dataset need to be transfered on T2 Storage site
+# MagnetField: 4T
+# Filtered on CastorMuon HLT path:
+#     HLT_L1Tech59_CastorMuon_v1
 sam["data_Cosmics_MuonHLTSkim_HI2015E_4T"]={}
 sam["data_Cosmics_MuonHLTSkim_HI2015E_4T"]["crabJobs"]=232
 sam["data_Cosmics_MuonHLTSkim_HI2015E_4T"]["GT"]='75X_dataRun2_Prompt_ppAt5TeV_v0'
@@ -92,6 +132,15 @@ sam["data_Cosmics_MuonHLTSkim_HI2015E_4T"]["XS"]=-1
 sam["data_Cosmics_MuonHLTSkim_HI2015E_4T"]["pathPAT"]='/XXXTMFPAT/tier2/store/user/cwohrman/CFF/CastorMuon/Cosmics/MuonAna_0T_20150717_data_Cosmics_MuonHLTSkim_2015E_4T/151209_200510/0000//'
 sam["data_Cosmics_MuonHLTSkim_HI2015E_4T"]["DS"]='/Cosmics/HIRun2015-v1/RAW'
 
+
+# CFF Skim using interfill data from run HI 2015E
+# with changed interfill HV set to pp-HV
+# This data is stored in the Cosmic dataset therefore it needed to run 
+# on RAW data (Cosmics/RECO has no Castor RecHit producer included). 
+# Dataset need to be transfered on T2 Storage site
+# MagnetField: 4T
+# Filtered on CastorMuon HLT path:
+#     HLT_L1Tech59_CastorMuon_v1
 sam["data_Cosmics_MuonHLTSkim_HI2015E_ppHVInterfill"]={}
 sam["data_Cosmics_MuonHLTSkim_HI2015E_ppHVInterfill"]["crabJobs"]=232
 sam["data_Cosmics_MuonHLTSkim_HI2015E_ppHVInterfill"]["GT"]='75X_dataRun2_Prompt_ppAt5TeV_v0'
